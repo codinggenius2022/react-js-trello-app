e986b8a79f795aac2e1d5eb202336a6ba1b06e0a
Abass Zakari <codinggenius2022@gmail.com>
2024-06-08 05:50:01 +0000

    video 2 design base board

--- src/App.js
+++ src/App.js
@@ -2,8 +2,123 @@
 
 function App() {
   return (
-    <div className="App">
-      <h1>Trello App in MERN</h1>
+    <div className="trello-master">
+      <nav className="navbar app">App bar</nav>
+      <nav className="navbar board">Board bar</nav>
+      <div className="board-columns">
+        <div className="column">
+          <header className="header">Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://images.pexels.com/photos/25328876/pexels-photo-25328876/free-photo-of-restaurant-a-paris.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" />
+              Design & Research
+            </li>
+            <li>item 1</li>
+            <li>item 2</li>
+            <li>item 3</li>
+            <li>item 1</li>
+            <li>item 2</li>
+            <li>item 3</li>
+          </ul>
+          <footer className="footer">Add a New Card</footer>
+        </div>
+        <div className="column">
+          <header className="header">Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://images.pexels.com/photos/25328876/pexels-photo-25328876/free-photo-of-restaurant-a-paris.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" />
+              Design & Research
+            </li>
+            <li>item 1</li>
+            <li>item 2</li>
+            <li>item 3</li>
+            <li>item 1</li>
+            <li>item 2</li>
+            <li>item 3</li>
+          </ul>
+          <footer className="footer">Add a New Card</footer>
+        </div>
+        <div className="column">
+          <header className="header">Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://images.pexels.com/photos/25328876/pexels-photo-25328876/free-photo-of-restaurant-a-paris.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" />
+              Design & Research
+            </li>
+            <li>item 1</li>
+            <li>item 2</li>
+            <li>item 3</li>
+            <li>item 1</li>
+            <li>item 2</li>
+            <li>item 3</li>
+          </ul>
+          <footer className="footer">Add a New Card</footer>
+        </div>
+        <div className="column">
+          <header className="header">Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://images.pexels.com/photos/25328876/pexels-photo-25328876/free-photo-of-restaurant-a-paris.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" />
+              Design & Research
+            </li>
+            <li>item 1</li>
+            <li>item 2</li>
+            <li>item 3</li>
+            <li>item 1</li>
+            <li>item 2</li>
+            <li>item 3</li>
+          </ul>
+          <footer className="footer">Add a New Card</footer>
+        </div>
+        <div className="column">
+          <header className="header">Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://images.pexels.com/photos/25328876/pexels-photo-25328876/free-photo-of-restaurant-a-paris.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" />
+              Design & Research
+            </li>
+            <li>item 1</li>
+            <li>item 2</li>
+            <li>item 3</li>
+            <li>item 1</li>
+            <li>item 2</li>
+            <li>item 3</li>
+          </ul>
+          <footer className="footer">Add a New Card</footer>
+        </div>
+        <div className="column">
+          <header className="header">Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://images.pexels.com/photos/25328876/pexels-photo-25328876/free-photo-of-restaurant-a-paris.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" />
+              Design & Research
+            </li>
+            <li>item 1</li>
+            <li>item 2</li>
+            <li>item 3</li>
+            <li>item 1</li>
+            <li>item 2</li>
+            <li>item 3</li>
+          </ul>
+          <footer className="footer">Add a New Card</footer>
+        </div>
+        <div className="column">
+          <header className="header">Brainstorm</header>
+          <ul>
+            <li>
+              <img src="https://images.pexels.com/photos/25328876/pexels-photo-25328876/free-photo-of-restaurant-a-paris.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" />
+              Design & Research
+            </li>
+            <li>item 1</li>
+            <li>item 2</li>
+            <li>item 3</li>
+            <li>item 1</li>
+            <li>item 2</li>
+            <li>item 3</li>
+          </ul>
+          <footer className="footer">Add a New Card</footer>
+        </div>
+      </div>
     </div>
   );
 }
